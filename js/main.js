ffe9b0bf8a08caa108a0047039c2f8eebbe81773
giri68 <giri.pdt@gmail.com>
2017-07-29 12:25:24 -0700

    created game logic

---
+++ js/main.js
@@ -0,0 +1,9 @@
+//to check javascript file is connected to index.html file
+console.log("up and running");
+//creating variable for 4 cards
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardThree);
