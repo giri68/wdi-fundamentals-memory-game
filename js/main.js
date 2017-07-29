4bd4880f58afc43591b2a3d24671d3aec127dbb1
giri68 <giri.pdt@gmail.com>
2017-07-29 14:32:17 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
 //to check javascript file is connected to index.html file
 console.log("up and running");
 //creating variable for 4 cards
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+
+//console.log("User flipped " + cardOne);
+//console.log("User flipped " + cardThree);
+//created array of four cards
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardsInPlay[0]);
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardsInPlay[1]);
+//checking the equality between two cards in array to find out match
+if (cardsInPlay.length === 2){
+  if (cardsInPlay[0] === cardsInPlay[1]){
+    alert("You found a match!");
+  } else {
+    alert("Sorry try again.");
+  }
+}
