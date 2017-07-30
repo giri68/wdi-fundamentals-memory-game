fce2ad989beb66f669eb6a4cad8e3f4a962ab80f
giri68 <giri.pdt@gmail.com>
2017-07-29 17:46:01 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -4,8 +4,29 @@ console.log("up and running");
 
 //console.log("User flipped " + cardOne);
 //console.log("User flipped " + cardThree);
-//created array of four cards
-var cards = ["queen", "queen", "king", "king"];
+//created array of four cards objects with properties
+var cards = [
+  {
+  rank: "queen",
+  suit: "hearts",
+  cardImage: "images/queen-of-hearts.png"
+  },
+  {
+  rank: "queen",
+  suit: "diamonds",
+  cardImage: "images/queen-of-diamonds.png"
+  },
+  {
+  rank: "king",
+  suit: "hearts",
+  cardImage: "images/king-of-hearts.png"
+  },
+  {
+  rank: "king",
+  suit: "diamonds",
+  cardImage: "images/king-of-diamonds.png"
+  }
+];
 var cardsInPlay = [];
 //created function to check flipped two card matches or not
 var checkForMatch = function(){
@@ -17,11 +38,14 @@ var checkForMatch = function(){
     }
 //  }
 }
-//created function to add flipped card in the new empty array
+//created function to add flipped card name in the new empty array
 var flipCard = function(cardId){
-  console.log("User flipped " + cards[cardId])
-  //checking the equality between two cards in array to find out match
-cardsInPlay.push(cards[cardId]);
+  console.log("User flipped " + cards[cardId].rank)
+
+cardsInPlay.push(cards[cardId].rank);
+console.log(cards[cardId].suit);
+console.log(cards[cardId].cardImage);
+//checking the equality between two cards name in array to find out match
 checkForMatch();
 }
 //called the function
