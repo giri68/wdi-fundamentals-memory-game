217ca60670306e92fe048334509c16fc73c02555
giri68 <giri.pdt@gmail.com>
2017-07-29 23:41:14 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -30,24 +30,44 @@ var cards = [
 var cardsInPlay = [];
 //created function to check flipped two card matches or not
 var checkForMatch = function(){
-//  if (cardsInPlay.length === 2){
+
+//checking condition to match two cards
+  if (cardsInPlay.length === 2){
     if (cardsInPlay[0] === cardsInPlay[1]){
       alert("You found a match!");
     } else {
       alert("Sorry try again.");
     }
-//  }
+ }
 }
 //created function to add flipped card name in the new empty array
-var flipCard = function(cardId){
+var flipCard = function(){
+  //taking data-id value as cardId
+var cardId = this.getAttribute("data-id");
   console.log("User flipped " + cards[cardId].rank)
-
+//pushing the flip card in cardsInPlay array
 cardsInPlay.push(cards[cardId].rank);
 console.log(cards[cardId].suit);
 console.log(cards[cardId].cardImage);
-//checking the equality between two cards name in array to find out match
+//setting the value of src attribute of flipped card as cardImage
+this.setAttribute("src", cards[cardId].cardImage);
+//checking the equality between two cards rank in array to find out match
 checkForMatch();
 }
-//called the function
-flipCard(0);
-flipCard(2);
+//created board of cards with images
+var createBoard = function(){
+  //looping through each elements of cards array
+  for (var i = 0; i < cards.length; i++){
+    //created img element
+    var cardElement = document.createElement("img");
+    //setting attribute src to every card and assigned value as back.img
+    cardElement.setAttribute("src", "images/back.png");
+    //setting attribute data-id to every card and assigned value as i
+    cardElement.setAttribute("data-id", i);
+    //adding event listener on each card on click event
+    cardElement.addEventListener("click", flipCard);
+    //appending card to the div of id game-board
+    document.getElementById("game-board").appendChild(cardElement);
+  }
+}
+createBoard();
